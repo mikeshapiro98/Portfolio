3b28bfcd7e3fe9acbe3394a0287b26f597237510
Mike Shapiro <michaelshapiro98@gmail.com>
2018-06-05 22:30:14 -0700

    Add 21 game

---
+++ TwentyOne/Dealer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Dealer
+    {
+        public string Name { get; set; }
+        public Deck Deck { get; set; }
+        public int Balance { get; set; }
+
+
+        public void Deal(List<Card> Hand)
+        {
+            Hand.Add(Deck.Cards.First());
+            Console.WriteLine(Deck.Cards.First().ToString() + "\n");
+            Deck.Cards.RemoveAt(0);
+
+
+        }
+
+
+    }
+
+}
+
+//YOU WOULDN'T INHERIT DECK HERE BECUASE INHARITANCE IS A 'IS A' RELATIONSHIP, NOT A 'HAS A' RELATIONSHIP. TWENTYONE IS A GAME A DEALER HAS A DECK.
+
+    //SO INSTEAD ITS A CLASS PROPERTY
+    //AIR ON THE SIDE OF PROPERTIES 
