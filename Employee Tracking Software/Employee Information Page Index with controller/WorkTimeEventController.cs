efc8f5cd4c6a369c8bee35dd8b824ad7ba5f4362
Mike Shapiro <michaelshapiro98@gmail.com>
2018-07-20 10:44:48 -0700

    Adding Employee Tracking Software To Portfolio

---
+++ Employee Tracking Software/Employee Information Page Index with controller/WorkTimeEventController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using ScheduleUsers.Areas.Employer.ViewModels;
+using ScheduleUsers.Models;
+using ScheduleUsers.ViewModels;
+
+namespace ScheduleUsers.Controllers
+{
+    [Authorize]
+    public class WorkTimeEventController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        
+        // GET: WorkTimeEvent
+        //[Authorize(Roles = "Admin")]
+      [HttpPost]
+        public ActionResult Index(EventListVm EVMView, string DateFilterSort, string HoursSorter)
+        {
+            //Sets the time of end date at 11:59:59 PM in order to account for events taking place on that day
+            EVMView.DisplayEndDate = EVMView.DisplayEndDate.Value.AddDays(1).AddSeconds(-1);
+            // Grabs the current user ID
+            var userId = User.Identity.GetUserId();
+            // Grabs all events in Db that have the same user ID as the one logging in
+            ApplicationUser user = db.Users.Find(userId);
+            //Checking for clocked in status
+            bool ClockedInStatus = db.WorkTimeEvents.Any(x => x.Id == userId && !x.End.HasValue);
+            //Begin query for selected dates on index page. 
+            //This IF statement accounts for the user putting in a start date that is after the end date. This switches their values. 
+            if (EVMView.DisplayBeginDate > EVMView.DisplayEndDate)
+            {
+                var temp = EVMView.DisplayBeginDate;
+                EVMView.DisplayBeginDate = EVMView.DisplayEndDate.Value;
+                EVMView.DisplayEndDate = temp;
+            }            
+            //This variable is taking in selected dates from the user and and ensuring that the dates are filtered correctly.
+            var Dates = db.WorkTimeEvents.Where(w => w.User.Id == userId).Where(w =>
+                                                              w.End <= EVMView.DisplayEndDate &&
+                                                              w.End >= EVMView.DisplayBeginDate ||
+                                                              w.Start <= EVMView.DisplayEndDate &&
+                                                              w.Start >= EVMView.DisplayBeginDate ||
+                                                              w.Start <= EVMView.DisplayBeginDate &&
+                                                              w.End >= EVMView.DisplayEndDate
+                                                              ).OrderBy(w => w.Start);
+            EventListVm EVM = new EventListVm(workTime: Dates.ToList(), user: user, ClockedIn: ClockedInStatus, Start: EVMView.DisplayBeginDate, End: EVMView.DisplayEndDate);
+            //Switch statement for sorting header columns by date
+            if (DateFilterSort != "update")
+            {
+                    //Sorting method by EventTotalHours(Total hours worked) of shifts
+                    if (HoursSorter != null)
+                    {
+                        HoursSorter = HoursSorter != "HoursSort" ? "HoursSort" : "HoursSortDec";
+                        switch(HoursSorter)
+                        {
+                            case "HoursSort":
+                                EVM.EventVms = EVM.EventVms.OrderBy(w => w.EventHoursWorked).ToList();
+                                break;
+                            case "HoursSortDec":
+                                EVM.EventVms = EVM.EventVms.OrderByDescending(w => w.EventHoursWorked).ToList();
+                                break;
+                        }
+                        ViewBag.HoursSortParm = HoursSorter;
+                    }
+                    else if(DateFilterSort != null)
+                    {
+                        //Sorting method by Start/End dates of shifts
+                        DateFilterSort = DateFilterSort != "date_desc" ? "date_desc" : "Date";
+                        switch (DateFilterSort)
+                        {
+
+                            case "Date":
+                                EVM.EventVms = EVM.EventVms.OrderBy(d => d.EventTimeIn).ToList();
+                                break;
+                            case "date_desc":
+                                EVM.EventVms = EVM.EventVms.OrderByDescending(d => d.EventTimeIn).ToList();
+                                break;
+
+                        }
+                        ViewBag.DateSortParm = DateFilterSort;                       
+                    }
+
+            }
+            return View("Index", EVM);
+        }
+
+        // GET: WorkTimeEvent
+        //[Authorize(Roles = "Admin")]
+        public ActionResult Index()
+        {
+
+            // Grabs the current user ID
+            var userId = User.Identity.GetUserId();
+            ApplicationUser user = db.Users.Find(userId);
+            bool ClockedInStatus = db.WorkTimeEvents.Any(x => x.Id == userId && !x.End.HasValue);
+            //Sorting the order to pre-populate the table with dates in ascending oerder
+            DateTime DisplayBeginDate = DateTime.Now.AddDays(-14);
+            DateTime DisplayEndDate = DateTime.Today.AddDays(1).AddSeconds(-1);
+            //This variable is taking in selected dates from the user and and ensuring that the dates are filtered correctly and sorted.
+            var Sorting = db.WorkTimeEvents.Where(w => w.User.Id == userId).Where(w =>
+                                                              w.End <=  DisplayEndDate &&
+                                                              w.End >=  DisplayBeginDate ||
+                                                              w.Start <=DisplayEndDate &&
+                                                              w.Start >=DisplayBeginDate ||
+                                                              w.Start <=DisplayBeginDate &&
+                                                              w.End >=  DisplayEndDate
+                                                              ).OrderBy(w => w.Start);
+            EventListVm EVM = new EventListVm(workTime: Sorting.ToList(), user: user, ClockedIn: ClockedInStatus);
+            return View(EVM);
+        }
+
+        
+    }
+}
---
+++ Employee Tracking Software/ViewModel example/EventListVm.cs
@@ -0,0 +1,154 @@
+﻿using ScheduleUsers.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ScheduleUsers.ViewModels
+{
+
+    //Instantiating EventList View Model
+    public class EventListVm
+
+    {
+        /// <summary>
+        /// The Id of the current user
+        /// </summary>
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// User Clock in date and time for display
+        /// </summary>
+        [DisplayName("Shift Begin Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DisplayBeginDate { get; set; }
+
+        /// <summary>
+        /// User Clock Out date and timer for display
+        /// </summary>
+        [DisplayName("Shift End Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime? DisplayEndDate { get; set; }
+
+        /// <summary>
+        /// List of Pay periods for display
+        /// </summary>
+        //public List<PayPeriod> PayPeriods { get; set; }
+
+        /// <summary>
+        /// List of properties from EventVm to populate object
+        /// </summary>
+        public List<EventVm> EventVms { get; set; }
+
+        /// <summary>
+        /// Status of whether or not the user is clocked in
+        /// </summary>
+        [DisplayName("Current Working Status")]
+        public bool ClockedIn { get; set; }
+
+        /// <summary>
+        /// Total hours worked by a user over a given timespan
+        /// </summary>
+        [DisplayName("Total Hours")]
+        public TimeSpan EventsTotalHours { get; set; }
+
+        /// <summary>
+        /// Expected gross wages for a user
+        /// </summary>
+        [DisplayName("Gross Projected Pay")]
+        public decimal GrossExpectedPay { get; set; }
+
+
+
+        public EventListVm(List<WorkTimeEvent> workTime, ApplicationUser user)
+            : this(workTime, user, DateTime.Now.AddDays(-14), DateTime.Today.AddDays(1).AddSeconds(-1), false)
+        {
+        }
+
+        public EventListVm(List<WorkTimeEvent> workTime, ApplicationUser user, bool ClockedIn)
+            : this(workTime, user, DateTime.Now.AddDays(-14), DateTime.Today.AddDays(1).AddSeconds(-1), ClockedIn)
+        {
+
+        }
+        public EventListVm()
+        {
+
+        }
+        public EventListVm(List<WorkTimeEvent> workTime, ApplicationUser user, DateTime Start, DateTime? End, bool ClockedIn)
+        {
+            this.UserId = user.Id;
+            this.DisplayBeginDate = Start;
+            this.DisplayEndDate = End;
+            this.EventVms = new List<EventVm>();
+            this.ClockedIn = ClockedIn;
+
+
+            for (var i = 0; i < workTime.Count; i++)
+            {
+                EventVm EVM = new EventVm(workTime[i]);
+                EventsTotalHours += EVM.EventHoursWorked;
+                EventVms.Add(EVM);
+            }
+            GrossExpectedPay = Convert.ToDecimal(EventsTotalHours.TotalMinutes / 60) * user.HourlyPayRate;
+
+        }
+    }
+    //Instansiation of Event View Model for passing to EventList View Model
+    public class EventVm
+    {
+        /// <summary>
+        /// Event Clock in date and time for user.
+        /// </summary>
+        [DisplayName("Shift Begin Date")]
+        public DateTime EventTimeIn{ get; set; }
+
+        /// <summary>
+        /// Event Clock out data and time for user.
+        /// </summary>
+        [DisplayName("Shift End Date")]
+        public DateTime? EventTimeOut { get; set; }
+
+        /// <summary>
+        /// Event Notes associated with current user
+        /// </summary>
+        [DisplayName("Shift Notes")]
+        public string EventNote { get; set; }
+
+        /// <summary>
+        /// Event Type of work for user. 
+        /// </summary>
+        //public string EventWorkType { get; set; }
+
+        /// <summary>
+        /// Event Amount of hours the user worked
+        /// </summary>
+        [DisplayFormat(DataFormatString = "{0:%d}" + "d " + "{0:%h}" + "h " + "{0:%m}" + "m", ApplyFormatInEditMode = true)]
+        [DisplayName("Total Shift Length")]
+        public TimeSpan EventHoursWorked{ get; set; }
+
+        /// <summary>
+        /// Event Id for individualization 
+        /// </summary>
+        public string Id { get; set; }
+
+        public EventVm(/*EventListVm EVM*/) { }
+        public EventVm(WorkTimeEvent WTE)
+        {            
+            this.Id = WTE.Id;
+            this.EventTimeIn = WTE.Start;
+            this.EventTimeOut = WTE.End;
+            this.EventNote = WTE.Note;
+            if(EventTimeOut != null)
+            {
+                this.EventHoursWorked = EventTimeOut.Value - EventTimeIn;
+            }
+          
+        }
+
+    }
+
+  
+}
